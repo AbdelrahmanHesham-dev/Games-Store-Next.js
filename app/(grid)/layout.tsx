6c95f46705f4ed1d83203fc8e7a5b5314d582340
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-12-25 23:04:43 +0200

    fix 3

--- app/(grid)/layout.tsx
+++ app/(grid)/layout.tsx
@@ -2,7 +2,6 @@ import GridContainer from "../components/defaults/GridContainer";
 import SideBar from "../components/nav/SideBar";
 import NavBar from "../components/nav/NavBar";
 import MaxWidthWrapper from "../components/defaults/MaxWidthWrapper";
-import ButtonGradient from "../components/ButtonGradient";
 
 export default function RootLayout({
   children,
@@ -11,7 +10,6 @@ export default function RootLayout({
 }>) {
   return (
     <main className="dark background grid min-h-screen h-full">
-      <ButtonGradient />
       <GridContainer className="background" cols={12}>
         <SideBar />
         <MaxWidthWrapper className="col-span-full lg:col-span-10">
--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,17 +1,12 @@
 "use client";
 import React from "react";
 import Search from "../Search";
-import ButtonGame from "../defaults/ButtonGame";
 
 const NavBar = () => {
   return (
     <nav>
       <header className="flex justify-between items-center">
         <Search />
-        <div className="flex items-center gap-3">
-          <ButtonGame link="/login" text="Login" />
-          <ButtonGame link="/signup" text="Sign Up" />
-        </div>
       </header>
     </nav>
   );
