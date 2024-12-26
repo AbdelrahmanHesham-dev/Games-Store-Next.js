f0e9653e229c3cef5579b9d64052c635fa127a9b
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-12-26 15:32:20 +0200

    Add Spinner

--- app/components/defaults/ButtonGame.tsx
+++ app/components/defaults/ButtonGame.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ButtonSvg from "../ButtonSvg";
 import Link from "next/link";
+import Spinner from "../defaults/Spinner";
 
-const ButtonGame = ({ text, link }: { text: string; link?: string }) => {
+const ButtonGame = ({
+  text,
+  link,
+  disabled,
+}: {
+  text: string;
+  link?: string;
+  disabled?: boolean;
+}) => {
   return (
-    <button className="hover:text-rose-400 duration-200 relative px-6 flex-initial gap-2 py-2 text-center m-auto">
+    <button
+      disabled={disabled}
+      className="hover:text-rose-400 duration-200 min-w-[100px] relative px-6 flex-initial gap-2 py-2 text-center m-auto"
+    >
       {ButtonSvg(false)}
       <span className="relative">
-        {link ? <Link href={link}>{text}</Link> : text}
+        {disabled ? <Spinner /> : link ? <Link href={link}>{text}</Link> : text}
       </span>
     </button>
   );
