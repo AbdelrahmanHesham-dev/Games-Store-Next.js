971cd47d5f697289ac75036ca7f5ff2974e09fc7
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-12-26 14:56:36 +0200

    fix

--- app/components/defaults/ButtonGame.tsx
+++ app/components/defaults/ButtonGame.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ButtonSvg from "../ButtonSvg";
 
-const ButtonGame = ({ text, link }: { text: string; link?: string }) => {
+const ButtonGame = ({ text }: { text: string }) => {
   return (
     <button className="relative px-6 flex-initial gap-2 py-3 text-center m-auto">
       {ButtonSvg(false)}
