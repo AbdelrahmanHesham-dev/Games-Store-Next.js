5355404177609c8e215141d5f922f152eee50b26
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-12-27 17:00:52 +0200

    add favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={` ${montserrat.className} dark antialiased`}>
         {children}
       </body>
