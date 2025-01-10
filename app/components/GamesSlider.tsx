23014e82573721cf797192f96f502cd9ab23168f
Abdelrahman <abdelrahmanhesham477@gmail.com>
2025-01-10 17:28:20 +0200

    fix eslint config

--- app/components/GamesSlider.tsx
+++ app/components/GamesSlider.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const GamesSlider = ({
-  games,
+  games = [],
   slidesPerView,
   big,
 }: {
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,11 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+const config = {
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
+
+
 export default eslintConfig;
